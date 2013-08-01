cc032af58bc392ef769ea2230c66556c50109a02
Hugo Vinícius Alves Soares <hvasoares@gmail.com>
2013-08-01 00:29:22 -0300

    more house cleaning post-composer

--- src/hvasoares/arrayredispersistence/GlueCode.php
+++ src/hvasoares/arrayredispersistence/GlueCode.php
@@ -1,23 +1,6 @@
 <?php
 namespace hvasoares\arrayredispersistence;
-
-require_once 'TranslationStrategy.php';
-require_once 'ArrayToModelTransforming.php';
-require_once 'CleanSchemaState.php';
-require_once 'DefaultTranslationStrategy.php';
-require_once 'InstantiatedSchema.php';
-require_once 'Mapper.php';
-require_once 'Model.php';
-require_once 'ModelToArrayTransforming.php';
-require_once 'Persistence.php';
-require_once 'Repository.php';
-require_once 'SchemaSettedState.php';
-require_once 'TransientState.php';
-require_once 'StateBuilder.php';
-require_once 'ModelProxyState.php';
-if(!class_exists('hvasoares\commom\Registry'))
-	require_once 'vendor/hvasoares/commons/Registry.php';
-use hvasoares\commom\Registry;
+use hvasoares\commons\Registry;
 class GlueCode{
 	public function getRegistry($top=null){
 		$reg = new Registry($top);
